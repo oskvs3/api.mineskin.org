e165728657d71308b83772c1992661eb651bcb14
oskvs3 <86385347+oskvs3@users.noreply.github.com>
2021-06-25 16:50:15 +0200

    Update Database.php

--- internal/Database.php
+++ internal/Database.php
@@ -1,29 +1,53 @@
-<?php
-function db() {
-    $json = json_decode(file_get_contents("../internal/mongo.json"));
-    $host = $json->host;
-    $port = $json->port;
-    $database = $json->database;
-    $user = $json->login->user;
-    $pass = $json->login->pass;
-    $login_db = $json->login->db;
+  
+import * as mongoose from "mongoose";
+import { ConnectOptions, Mongoose } from "mongoose";
+import * as Sentry from "@sentry/node";
+import { MineSkinConfig } from "../typings/Configs";
+import tunnel = require("tunnel-ssh");
 
-    $auth = "";
-    if (!is_null($user) && !is_null($pass)) {
-        $auth = "$user:$pass@";
-    }
-    try {
-        $mongo = new MongoClient ("mongodb://$auth$host:$port/$login_db");
-        $db = $mongo->selectDB($database);
-    } catch (Exception $e) {
-        exit("Database connection failed");
-    }
-
-    unset ($json);
-    unset ($user);
-    unset ($pass);
-
-    return $db;
+export function connectToMongo(config: MineSkinConfig): Promise<Mongoose> {
+    return new Promise<Mongoose>((resolve, reject) => {
+        if (config.mongo.useTunnel) {
+            console.log("Establishing SSH Tunnel to " + config.mongo.tunnel.host + "...");
+            tunnel(config.mongo.tunnel, (err, server) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                connectMongo(config).then(resolve).catch(reject);
+            })
+        } else {
+            connectMongo(config).then(resolve).catch(reject);
+        }
+    })
 }
 
-?>
+async function connectMongo(config: MineSkinConfig) {
+    // Connect to DB
+
+    const options: ConnectOptions = {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        autoIndex: false
+    };
+
+    let m: Mongoose;
+    if (config.mongo.url) {
+        console.log("Connecting to mongodb...");
+        m = await mongoose.connect(config.mongo.url, options);
+    } else {
+        console.log("Connecting to mongodb://" + ((config.mongo.user || "admin") + ":*****" + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
+        m = await mongoose.connect("mongodb://" + ((config.mongo.user || "admin") + ":" + (config.mongo.pass || "admin") + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")), options);
+    }
+    console.info("MongoDB connected!");
+
+    mongoose.connection.on("error", err => {
+        Sentry.captureException(err);
+        console.warn("Mongo connection error, restarting app");
+        setTimeout(() => {
+            process.exit(1);
+        }, 10000);
+    })
+
+    return m;
+}
